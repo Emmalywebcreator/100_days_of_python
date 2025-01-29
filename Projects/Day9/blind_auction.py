fe5df94e4cd6b16674a41a63a3e3e3ae9b81b2f5
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-01-29 02:22:30 +0100

    updated the blind auction function

--- Projects/Day9/blind_auction.py
+++ Projects/Day9/blind_auction.py
@@ -3,28 +3,40 @@ import os
 def clear():
     """Clears the terminal screen based on the operating system"""
     os.system('cls' if os.name == 'nt' else 'clear')
-    
-    
-bids = {}
-bidding_continue = False
 
-def find_highest_bidder(bidding_record):
+# TODO-3: Whether if new bids need to be added
+# TODO-2: Save data into dictionary {name: price}
+# TODO-1: Ask the user for input
+
+
+should_continue = True
+bid_dict = {}
+
+def find_highest_bidder(bids):
     highest_bid = 0
-    winner = ""
-    for bidder in bidding_record:
-        bid_amount = bidding_record[bidder]
+    winner = ''
+    for bidder in bids:
+        bid_amount = bids[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
     print(f"The winner is {winner} with a bid of {highest_bid}")
-    
-while bidding_continue:
-    name = input("What is your name?:\n")
-    price = int(input("How much do you bid?:\n"))
-    bids[name] = price
-    should_continue = input("Are there still other bidders?\n yes or no")
-    if should_continue == "no":
-        bidding_continue = False
-        find_highest_bidder(bids)
-    elif should_continue == "yes":
+
+while should_continue:
+    name = input("What is your name?\n")
+    price = int(input("How much are you bidding?\n"))
+    bid_dict[name] = price
+
+    new_bidder = input("Are ther any other bidders? 'yes' or 'no'\n").lower()
+    if new_bidder == "no":
+        should_continue = False
+        find_highest_bidder(bid_dict)
+    elif new_bidder == "yes":
+        should_continue = True
         clear()
+    
+
+
+
+# TODO-4: Compare bids in dictionary
+
