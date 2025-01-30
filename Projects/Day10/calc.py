f473da8ececac428c3d585aab84d019756c80004
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-01-30 15:13:54 +0100

    added More feature to the calculator

--- Projects/Day10/calc.py
+++ Projects/Day10/calc.py
@@ -1,42 +1,53 @@
-def add(n1, n2):
-    return n1 + n2
-def sub(n1, n2):
-    return n1 - n2
-def mul(n1, n2):
-    return n1 * n2
-def div(n1, n2):
-    return n1 / n2
-def mod(n1, n2):
-    return n1 % n2
+import os
 
-operators = {
-    "+": add,
-    "-": sub,
-    "*": mul,
-    "/": div,
-    "%": mod,
-}
-num1 = float(input("What is the first number?\n"))
+def clr():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def add(n1, n2): return n1 + n2
+def sub(n1, n2): return n1 - n2
+def mul(n1, n2): return n1 * n2
+def div(n1, n2): return n1 / n2 if n2 != 0 else "Error, Division by 0 not allowed"
+def mod(n1, n2): return n1 % n2 if n2 != 0 else "Error, Modulo by 0 not allowed"
 
-print("Available operoator:", " ".join(operators.keys()))
+opps = { "+": add, "-": sub, "*": mul, "/": div, "%": mod,}
 
-choose_operator = input("Pick an operator: ")
-num2 = float(input("What is the second number?\n"))
-
-if choose_operator == "+":
-    print(f"{num1} + {num2} = {operators['+'](num1, num2)}")
-elif choose_operator == "-":
-    print(f"{num1} - {num2} = {operators['-'](num1, num2)}")
-elif choose_operator == "*":
-    print(f"{num1} * {num2} = {operators['*'](num1, num2)}")
-elif choose_operator == "/":
-    print(f"{num1} / {num2} = {operators['/'](num1, num2)}")
-elif choose_operator == "%":
-    print(f"{num1} % {num2} = {operators['%'](num1, num2)}")
-
-
-
-
-
-
-
+while True:
+    try:
+        num1 = float(input("Enter the first number:\n"))
+    except ValueError:
+        print("Invalid Input: Please enter a valid number.")
+        continue
+    
+    while True:
+        print("Available opp:", " ".join(opps.keys()))
+        choose_operator = input("Choose an operator:\n")
+      
+        
+        try:
+            num2 = float(input("Enter the next number:\n"))
+        except ValueError:
+            print("Invalid Input: Please enter a valid number")
+        
+        if choose_operator in opps:
+            if choose_operator in ['/', '%'] and num2 == 0:
+                print("Division or modulo by 0 not allowed. Enter a valid number.")
+                continue
+            result = opps[choose_operator](num1, num2)
+            clr()
+            print(f"{num1} {choose_operator} {num2}  = {result}")
+        else:
+            print("Invalid operation. Please enter a valid operation")
+        
+        while True:
+            choice = input(f"Enter 'y' to continue with {result} 'n' to start afresh 'q' to quit:\n")
+            clr()
+            if choice == 'y':
+                num1 = result
+                break
+            elif choice == 'n':
+                break
+            elif choice == 'q':
+                print("Goodbye!")
+                exit()
+        if choice == 'n':
+                break     
