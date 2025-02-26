f802646d42b2c07c41b0a03ea8fbd5a447c63a0a
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-02-26 08:14:20 +0100

    turtle gui

---
+++ Projects/Day18/turtle_gui.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+tom = Turtle()
+tom.shape("turtle")
+tom.color("green")
+tom.forward(100)
+tom.right(90)
+tom.forward(100)
+tom.right(90)
+tom.forward(100)
+tom.right(90)
+tom.forward(100)
+
+screen = Screen()
+screen.exitonclick()
