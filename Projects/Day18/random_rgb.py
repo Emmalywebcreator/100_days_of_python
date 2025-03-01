6a28560b4e3ffc0476c42b22aaabe350a70f5524
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2025-03-01 22:14:07 +0100

    turtle making a random walk with random rgb

---
+++ Projects/Day18/random_rgb.py
@@ -0,0 +1,26 @@
+# turtle make a random walk with random rgb
+import random
+import turtle as t
+
+screen = t.Screen()
+t.colormode(255)
+jimmy = t.Turtle()
+jimmy.pensize(15)
+jimmy.speed("fastest")
+
+def random_colors():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+direction = [0, 90, 180, 270]
+
+for _ in range(200):
+    jimmy.color(random_colors())
+    jimmy.forward(30)
+    jimmy.right(random.choice(direction))
+
+
+screen.exitonclick()
